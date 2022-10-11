d9024ea0190e5d4eb15ad677c8335f1d21ab68a7
DineshBadugu <developer@dt7solutions.com>
2022-10-11 10:01:03 +0530

    fixed portfolio section

--- Sumafilmyart/static/js/portfolio.js
+++ Sumafilmyart/static/js/portfolio.js
@@ -2,10 +2,10 @@ var swiper = new Swiper(".port_video_slider", {
     slidesPerView: 3,
         spaceBetween: 30,
         freeMode: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
+        // pagination: {
+        //   el: ".swiper-pagination",
+        //   clickable: true,
+        // },
         autoplay: {
         delay: 5000,
         },
